6abc4a8f307f2c407f0e9e4f1d24ac388b4f84fa
Akayeshmantha <kaperera@mitrai.com>
2018-10-23 15:26:23 +0530

    adding unit test for invert axis and filling value

--- test/coordinatesHelper.test.js
+++ test/coordinatesHelper.test.js
@@ -1,7 +1,6 @@
 const coordinatesHelper = require('../coordinatesHelper');
 
 test('it should return the max Y vertice', () => {
-
     const vertices = {"textAnnotations":[{"boundingPoly":{
         "vertices":[
             {"x":262,"y":260},
@@ -13,3 +12,32 @@ test('it should return the max Y vertice', () => {
     expect(yMax).toBe(3486);
 });
 
+test('it should fill all undefined vertices x and y cordinates', () => {
+    const vertices = {"textAnnotations":[
+    {"boundingPoly":{
+        "vertices":[
+            {"x":262,"y":260},
+            {"x":1176,"y":260},
+            {"x":1176,"y":3486},
+            {"x":262,"y":3486}
+        ]
+    }},
+    {"boundingPoly":{
+        "vertices":[
+            {"x":262,"y":undefined},
+            {"x":1176,"y":260},
+            {"x":undefined,"y":3486},
+            {"x":262,"y":3486}
+        ]
+    }}]};
+
+    const matchingVal = [   
+        { x: 262, y: 3486 },
+        { x: 1176, y: 3226 },
+        { x: 0, y: 0 },
+        { x: 262, y: 0 } 
+    ];
+    const maxYVertice = 3486;
+    const invertedData = coordinatesHelper.invertAxis(vertices,maxYVertice);
+    expect(invertedData.textAnnotations[1].boundingPoly.vertices).toEqual(matchingVal);
+});
